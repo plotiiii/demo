ea71d551bef83b930cce3b75d669e7e76ca3de1d
plotiiii <48316745+plotiiii@users.noreply.github.com>
2019-11-28 11:08:20 +0800

    Delete demo03.py

--- demo03.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-wd=webdriver.Chrome(r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
-wd.get('http://f.python3.vip/webauto/sample1a.html')
-
-ele=wd.find_elements_by_css_selector('#t1>span,#t1>p')
-for element in ele:
-    print(element.text)
